c2a1028c1408ff381fe3a7b0e4cb42e3794a0fc2
M Groesbeck <m.groesbeck@gmail.com>
2020-02-27 12:56:48 -0800

    MVP will be complete when checkbox validation works.

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -3,35 +3,69 @@ import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-const UserForm = props => {
+const UserForm = ({values, touched, errors, status}) => {
+    const [user, setUser] = useState({});
+    useEffect (() => {
+        console.log("status has changed", status)
+        status && setUser(user => status)
+    },[status])
   return (
     <div className="user-form">
       <Form>
         <label htmlFor="name">
           Name:
           <Field id="name" type="text" name="name" placeholder="Name" />
+          {touched.name && errors.name && (<p className="errors">{errors.name}</p>)}
         </label>
         <label htmlFor="email">
           email:
           <Field id="email" type="text" name="email" placeholder="email" />
+          {touched.email && errors.email && (<p className="errors">{errors.email}</p>)}
         </label>
         <label htmlFor="password">
           Password:
           <Field id="password" type="password" name="password" placeholder="Password" />
+          {touched.password && errors.password && (<p className="errors">{errors.password}</p>)}
         </label>
         <label htmlFor="terms-of-service">
             Accept Terms of Service?
             <Field id="tos" type="checkbox" name="tos"/>
+            {touched.tos && errors.tos && (<p className="errors">{errors.tos}</p>)}
         </label>
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </Form>
+      {user.id && <ul key={user.id}>
+          <li>Name: {user.name}</li>
+          <li>email: {user.email}</li>
+          <li>Password: {user.password}</li>
+          <li>ToS accepted? {user.tos.toString()}</li>
+      </ul>}
     </div>
   );
 };
 
 const FormikUserForm = withFormik({
+    mapPropsToValues({name, email, password, tos}) {
+        return {
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            tos: tos || false
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().min(3, "Please enter a longer name.").required("Please enter a name."),
+        email: Yup.string().email("Please enter a valid email.").required("Email required."),
+        password: Yup.string().min(8, "Password must be at least 8 characters.").required("Please enter a password."),
+        tos: Yup.boolean().required("Please accept Terms of Service.")
+    }),
     handleSubmit(values, {setStatus, resetForm}){
-        console.log(values);
+        axios.post("https://reqres.in/api/users/", values)
+        .then( response => {
+            console.log("Post successful, ", response);
+            setStatus(response.data);
+            resetForm();
+        })
     }
 })(UserForm);
 
